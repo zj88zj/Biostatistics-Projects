1b39b26cdddb82140148e692219399615a3a122e
Jing Zhang <zj88zj@hotmail.com>
2019-01-14 01:30:41 -0500

    <report and codes>

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Biostatistics-Projects
-Biostatistics Project
+Prediction of Length of Stay (LOS) from Patients Data in Sweden
+
+
+Keywords: 
+Exploratory Data Analysis, Correlation, Survival Models, Log Rank Test, ANOVA, Linear Regression, Cox Proportional-Hazards Model, Weibull Regression Model, Variable Selection, Forward and Backward Stepwise, LASSO, Data Visialzation.
+
+Packages: 
+Hmisc, corrplot, survival, flexsurv, eha, graphics, grDevices, leaps, ggvis, lars, methods, stats, utils, datasets.
+
+
+Database contains the data of all patients hospitalized with depression in Sweden from 2008 to 2013. The variables are:
+"YEAR","AGE","RACE","FEMALE","LOS","NCHRONIC","DIED","ALCOHOL","ANEMDEF","ARTH","BLDLOSS","CHF","CHRNLUNG","COAG","DEPRESS","DRUG","HTN","LIVER","OBESE","RENLFAIL","LAMA","ULCER","VALVE","WGHTLOSS"
+
+We are interested in predicting the distribution of Length of Stay (LOS) as a function of demographics, disease variables and comorbidities. However, many of these patients left the hospital against medical advice (LAMA) so the observe value could be consider as a lower bound of the true LOS.
+
+
+Main Tasks:
+1. Explore the dataset for interesting patterns and relationships among the variables.
+2. Make assumption and find appropriate models.
+3. Find the most important factors that determine the length of stay.
+Compare a parametric model with a non-parametric model and show the
+differences. Compare Proportional Hazard vs Weibull models. Use variable selection models.
+4. Check the randomness of patients who left against medical advice.
+
---
+++ Rcode.R
@@ -0,0 +1,113 @@
+###### Exploratory Data Analysis ######
+
+names(dataFP) #column names
+summary(dataFP) #basic descriptive statistics
+table(dataFP$LOS) #frequency of Length of stay
+length(table(dataFP$LOS))
+hist(dataFP$LOS) #distributions of LOS
+
+##Correlationship among Variables##
+cor(dataFP)
+
+##Correlation Matrix with P-value##
+install.packages("Hmisc")
+library("Hmisc")
+rcorr(as.matrix(dataFP))
+
+##Displays for Correlation Matrices##
+install.packages("corrplot")
+library(corrplot)
+corrplot(cor(dataFP), type="upper", tl.srt=45, tl.col="black", tl.cex = 0.5)
+
+
+
+
+###### Kaplan-Meier Survival Curves ######
+
+library(survival)
+
+##Survival Models##
+plot(survfit(Surv(log(1+LOS),1-LAMA)~1,data=dataFP))
+plot(survfit(Surv(log(1+LOS),1-LAMA)~DIED,data=dataFP))
+
+##Log Rank Test##
+survdiff(Surv(log(1+LOS),1-LAMA)~AGE,data=dataFP)
+
+
+
+
+###### ANOVA and Linear Regression ######
+
+linearMod <- lm(LOS ~ ., data =dataFP)
+linearMod2 <- lm(log(1+LOS) ~ ., data =dataFP)
+summary(aov(LOS ~ ., data=dataFP))
+summary(linearMod2)
+AIC(linearMod2)
+BIC(linearMod2)
+
+##Residual Plots and QQplots##
+plot(linearMod2, las = 1)
+
+
+
+###### Cox Proportional-Hazards Model ######
+
+coxMod <- coxph(Surv(log(1+LOS), 1-LAMA) ~ ., data=dataFP)
+print(coxMod)
+summary(coxMod)
+plot(cox.zph(coxMod))
+
+
+
+###### Weibull Regression Model ######
+
+library(survival)
+weiMod <- survreg(Surv(log(2+LOS), 1-LAMA) ~ ., data=dataFP, dist="weibull")
+summary(weiMod)
+library(eha)
+weiMod2<- weibreg(Surv(log(2+LOS), 1-LAMA)~ ., data=dataFP)
+summary(weiMod2)
+plot(weiMod2)
+
+
+
+###### Variable Selection ######
+
+##Subset Selection##
+library(leaps)
+sel.full=regsubsets(log(1+LOS) ~ ., data=dataFP)
+summary(sel.full)
+library(ggvis)
+rsq <- as.data.frame(summary(sel.full)$rsq)
+names(rsq) <- "R2"
+rsq %>% 
+  ggvis(x=~ c(1:nrow(rsq)), y=~R2 ) %>%
+  layer_points(fill= ~ R2 ) %>%
+  add_axis("y", title="R2") %>% 
+  add_axis("x", title="Number of variables")
+
+##Forward & Backward Stepwise Selection##
+sel.f = regsubsets(log(1+LOS) ~ ., data=dataFP,nvmax=20, method ="forward")
+sel.b = regsubsets(log(1+LOS) ~ ., data=dataFP,nvmax=20, method ="backward")
+summary(sel.f)
+coef(sel.full ,8)
+coef(sel.f ,8)
+coef(sel.b ,8)
+
+##LASSO for Variable Selection##
+LOS <-dataFP$LOS
+lasso<-lars(x=as.matrix(dataFP), y=log(1+LOS), type='lasso', trace=FALSE, normalize=TRUE, intercept=TRUE)#define a lasso object
+plot(lasso)
+round(coef(lasso),2)
+predict.lars(object=lasso, s=0.85, mode='fraction', type='coefficients')
+absum<- sum(abs(summary(linearMod2)$coeff[-1,1]))
+t<-apply(abs(coef(lasso)),1,sum)
+s<-t/absum
+plot(s, coef(lasso)[,2],xlab='shinkage factor s', ylab='coefficient', axes=TRUE)#lasso plot
+lines(s,coef(lasso)[,4])
+lines(s,coef(lasso)[,5])
+lines(s,coef(lasso)[,6])
+lines(s,coef(lasso)[,12])
+lines(s,coef(lasso)[,16])
+lines(s,coef(lasso)[,17])
+lines(s,coef(lasso)[,21])
